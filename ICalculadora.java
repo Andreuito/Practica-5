2297728e21898d0e914b090519521d8c3c40bf66
Andreu S.G <91564971+Andreuito@users.noreply.github.com>
2022-05-15 16:30:06 +0200

    primer commit

---
+++ ICalculadora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Calculadora;
+
+/**
+ *
+ * @author andreus
+ */
+public interface ICalculadora {
+    public float suma(float num1, float num2);
+    public float resta(float num1, float num2);
+    public float division(float num1, float num2);
+    public float multiplicacion(float num1, float num2);
+}
